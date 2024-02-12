072efee06b1e67bc87077268daf7f9db87013615
elijahnikov <enkv@proton.me>
2024-02-12 21:24:09 +0000

    adding device and location cards

--- src/components/pages/grid/analytics/analytics-container.tsx
+++ src/components/pages/grid/analytics/analytics-container.tsx
@@ -1,29 +1,21 @@
 "use client";
 
-import { BarChart } from "@tremor/react";
-import { Card } from "@/lib/ui/card";
 import { api } from "@/trpc/react";
 import { useParams } from "next/navigation";
 import AnalyticsViewFilter from "./analytics-filter";
 import { useState } from "react";
-import groupAnalyticsData from "@/lib/groupAnalyticsData";
+import ClicksBarChart from "./bar-chart";
+import LocationChart from "./location-chart";
+import DeviceChart from "./device-chart";
 
 export default function AnalyticsContainer() {
   const [dateRange, setDateRange] = useState<string>("30 days");
   const { slug } = useParams() as { slug: string };
-  const { data, error, isLoading } = api.analytics.gridClicks.useQuery({
-    slug,
-    dateRange,
-  });
-
-  if (isLoading) {
-    return <></>;
-  }
-  if (error) {
-    return <></>;
-  }
-
-  const tData = groupAnalyticsData({ data, dateRange });
+  const { data, error, isLoading, isRefetching } =
+    api.analytics.gridClicks.useQuery({
+      slug,
+      dateRange,
+    });
 
   return (
     <div className="space-y-3">
@@ -31,21 +23,19 @@ export default function AnalyticsContainer() {
         rangeValue={dateRange}
         setRangeValue={setDateRange}
       />
-      <Card className="p-5">
-        <div className="mb-10">
-          <h1 className="text-sm text-slate-500">Clicks</h1>
-          <p className="text-3xl font-semibold">465</p>
+      <div className="grid w-full grid-cols-2 gap-5">
+        <div className="col-span-2">
+          {data && (
+            <ClicksBarChart
+              data={data}
+              dateRange={dateRange}
+              isLoading={isRefetching}
+            />
+          )}
         </div>
-        <BarChart
-          allowDecimals={false}
-          showLegend={false}
-          className="h-80"
-          data={tData}
-          index="date"
-          categories={["clicks"]}
-          yAxisWidth={40}
-        />
-      </Card>
+        <LocationChart />
+        <DeviceChart />
+      </div>
     </div>
   );
 }
